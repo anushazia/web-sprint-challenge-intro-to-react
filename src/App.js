31ddd006e820f21ffae28df1f36fb3a389a1833c
anushazia <anusha.zi@gmail.com>
2021-04-06 00:05:30 -0400

    got some stuff working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from "./components/Character"
+import Axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+ const [data, setData] = useState([])
+  
+ useEffect((data) => {
+  Axios
+  .get("https://swapi.dev/api/people/")
+  .then(res => {
+    setData(res.data)
+    console.log(res.data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.map(data => {
+        return <Character data={data}/>
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from 'react'
+import Axios from "axios"
+
+export default function Character (props) {
+    const { data } = props
+    
+return (
+    <div className='Characters container'>
+        <p>{Character.name} </p>
+    </div>
+)}
