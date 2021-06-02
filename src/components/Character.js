fbda0a0bcc85c3839095996168dfe22e4c4378fb
Anusha Zia <anushazia@Ammars-MacBook-Pro.local>
2021-06-01 22:04:46 -0400

    added styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,25 +1,62 @@
-// Write your Character component here
-import React from 'react'
-import { Card, CardTitle, Container, Row, Col } from 'reactstrap'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "../App.css";
+import styled from "styled-components";
 
+//styling for p elements
+const StyledParagraph = styled.p`
+	margin: 10px;
+`;
 
-function Character (props) {
-   console.log(props.characterArray, 'This is showing props')
-    
-    return (
-        <div>  
-      {props.characterArray.map((character) => {
-          return(
-              <Card key={character.created}>
-                <CardTitle>{character.name}</CardTitle>
-              </Card>
+//styling for buttons
+const StyledButton = styled.button`
+	padding: 7px;
+	border-radius: 10px;
+`;
 
+const Character = ({ charData }) => {
+	const [currentCharId, setCurrentCharId] = useState(null);
 
-          )
+	//function for "View Details"
+	const showDetails = (id) => {
+		setCurrentCharId(id);
+	};
 
-      })}
-        </div>
-    )
-}
+	//function for "Hide Details"
+	const hideDetails = () => {
+		setCurrentCharId(null);
+	};
 
-export default Character
+	return (
+		<div>
+			<h3> {charData.name} </h3>
+
+			{/* View Details button */}
+			<StyledButton onClick={() => showDetails(charData.name)}>
+				View details
+			</StyledButton>
+
+			{/* This will render the "More Details" div element when showDetails() is triggered thru clicking the View Details button */}
+			{currentCharId && (
+				<div>
+					<StyledParagraph>
+						Gender: {charData.gender} <br />
+						Birth Year: {charData.birthYear} <br />
+						Height: {charData.height} <br />
+						Hair Color: {charData.hair_color} <br />
+						Skin Color: {charData.skin_color} <br />
+						Eye Color: {charData.eye_color} <br />
+					</StyledParagraph>
+
+					{/* Hide Details button */}
+					<StyledButton onClick={() => hideDetails(null)}>
+						{" "}
+						Hide details{" "}
+					</StyledButton>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Character;
